abfc86aa9563cb00d5bff1c2e07532a7d02b219b
Artem Barinov <time.barinov@gmail.com>
2021-07-08 20:15:01 +0300

    updated linked_list

--- data_structure/linked_list/linked_list.cpp
+++ data_structure/linked_list/linked_list.cpp
@@ -7,7 +7,7 @@ public:
     };
     
     explicit LinkedList() : head(nullptr), tail(nullptr), size(0) {}
-
+    
     ~LinkedList();
 
     void PushFront(const T& value);
@@ -41,20 +41,19 @@ public:
     }
 
     inline T& operator [](int index) {
-        return Find(index)->value;
+        return IndexOf(index)->value;
     }
-private:
+
+private:    
     Node* head = nullptr;
     Node* tail = nullptr;      
     int size = 0;
 
-    Node* Find(int index) {
-        Node* tmp = head;
-        for(int i = 0; i < index; i++) {
-            tmp = tmp->next;
-        }
-        return tmp;
-    }
+    
+    int cached_index = 0;
+    Node* cached_node = nullptr;
+    
+    Node* IndexOf(int index);
 };
 
 template <typename T>
@@ -69,23 +68,23 @@ void LinkedList<T>::PushFront(const T& value) {
     if (size == 0) {
         PushBack(value);
     }
-    auto node = new Node{value};
-    node->next = head;
-    head = node;
+    auto new_node = new Node{value};
+    new_node->next = head;
+    head = new_node;
     size++;
 }
 
 template <typename T>
 void LinkedList<T>::PushBack(const T& value) {
-    auto node = new Node{value};
-    node->next = nullptr;
+    auto new_node = new Node{value};
+    new_node->next = nullptr;
     if (head) {
-        tail->next = node;
-        tail = node;
+        tail->next = new_node;
+        tail = new_node;
     }
     else {
-        head = node;
-        tail = node;
+        head = new_node;
+        tail = new_node;
     }
     size++;
 }
@@ -108,6 +107,8 @@ void LinkedList<T>::PopFront() {
         tail = nullptr;
         size = 0;
     }
+    cached_index = 0;
+    cached_node = nullptr;
 }
 
 template <typename T>
@@ -117,7 +118,7 @@ void LinkedList<T>::PopBack() {
     }
     
     if (size >= 2) {
-        Node* new_tail = Find(size - 2);
+        Node* new_tail = IndexOf(size - 2);
         delete(new_tail->next);
         new_tail->next = nullptr;
         tail = new_tail;
@@ -128,5 +129,30 @@ void LinkedList<T>::PopBack() {
         head = nullptr;
         tail = nullptr;
         size = 0;
+    }
+    cached_index = 0;
+    cached_node = nullptr;
+}
+
+template <typename T>
+typename LinkedList<T>::Node* LinkedList<T>::IndexOf(int index) {
+    if (cached_index < index && cached_node) {
+        Node* tmp = cached_node;
+        for(int i = cached_index; i < index; i++) {
+            tmp = tmp->next;                
+            cached_index++; 
+        }
+        cached_node = tmp;
+        return tmp;
+    }
+    else {
+        Node* tmp = head;
+        cached_index = 0;
+        for(int i = 0; i < index; i++) {
+            tmp = tmp->next;
+            cached_index++;
+        }
+        cached_node = tmp;
+        return tmp;
     }
 }
